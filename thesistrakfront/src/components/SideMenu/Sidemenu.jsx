d33f3cffb864aeb2a9b1b4221fa0e807339a806a
ProgramadorGod <pipenet12@gmail.com>
2024-06-06 12:52:57 -0500

    sidemenu upgraded a bit

--- thesistrakfront/src/components/SideMenu/Sidemenu.jsx
+++ thesistrakfront/src/components/SideMenu/Sidemenu.jsx
@@ -1,8 +1,8 @@
-import { RxAccessibility, RxHome, RxPerson } from "react-icons/rx";
+import { RxAccessibility, RxAspectRatio, RxBorderDashed, RxHome, RxMixerVertical, RxPerson, RxPieChart } from "react-icons/rx";
 import { BiAward, BiDesktop, BiFolderOpen } from "react-icons/bi";
 import { FaAddressBook, FaAddressCard } from "react-icons/fa";
-import { TbHomeFilled } from "react-icons/tb";
-import { PiPersonArmsSpreadBold } from "react-icons/pi";
+import { TbHexagonalPyramidPlus, TbHomeFilled } from "react-icons/tb";
+import { PiAirTrafficControl, PiBarbell, PiBarcode, PiCakeDuotone, PiGraphicsCard, PiGraphicsCardThin, PiIdentificationBadgeFill, PiPersonArmsSpreadBold } from "react-icons/pi";
 import { IoMdPerson } from "react-icons/io";
 
 import GlobalActivation from "../GlobalAc"
@@ -37,6 +37,19 @@ const Sidemenu = ({isActive}) => {
 
                 <button onClick={handleMyFilesClick} className="buttondiv" >
                     <BiFolderOpen className="itemlogo"/>
+                </button>
+
+
+                <button onClick={handleMyFilesClick} className="buttondiv" >
+                    <RxMixerVertical className="itemlogo"/>
+                </button>
+
+                <button onClick={handleMyFilesClick} className="buttondiv" >
+                    <TbHexagonalPyramidPlus className="itemlogo"/>
+                </button>
+
+                <button onClick={handleMyFilesClick} className="buttondiv" >
+                    <RxPieChart className="itemlogo"/>
                 </button>
                 
             </div>
