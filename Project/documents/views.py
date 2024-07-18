87eef7107f17c9b139f4a8261bec84ab432cdf8c
ProgramadorGod <pipenet12@gmail.com>
2024-07-17 20:09:26 -0500

    AllThesisInDataBase, now adding filter by pages to optimize performance

--- Project/documents/views.py
+++ Project/documents/views.py
@@ -1,16 +1,42 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Document, DocumentStage, DocumentType,Carrer
-from .serializers import DocumentSerializer, DocumentStageSerializer, DocumentTypeSerializer,CarrerSerializer
-from rest_framework.permissions import IsAuthenticated,AllowAny
-# Create your views here.
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.db.models import Q
+from .models import Document, DocumentStage, DocumentType, Carrer
+from .serializers import DocumentSerializer, DocumentStageSerializer, DocumentTypeSerializer, CarrerSerializer
 
+# Clase de paginación
+class DocumentPagination(PageNumberPagination):
+    page_size = 30
 
+# Vista personalizada para paginación, búsqueda y filtrado
+@api_view(["GET"])
+def document_list(request):
+    query = request.GET.get('query', '')
+    sort_by = request.GET.get('sort_by', 'title')  # Ordenar por título por defecto
+    documents = Document.objects.all()
+
+    if query:
+        documents = documents.filter(
+            Q(title__icontains=query) |
+            Q(authors__icontains=query)  # Modifica según los campos de tu modelo
+        )
+
+    documents = documents.order_by(sort_by)  # Ordenar por el campo especificado
+
+    paginator = DocumentPagination()
+    result_page = paginator.paginate_queryset(documents, request)
+    serializer = DocumentSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
+
+# ViewSets para otras vistas
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
     permission_classes = [AllowAny]
-
 
 class CarrerViewSet(viewsets.ModelViewSet):
     queryset = Carrer.objects.all()
--- thesistrakfront/src/components/files/files.jsx
+++ thesistrakfront/src/components/files/files.jsx
@@ -1,75 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./files.css";
-import axios from 'axios';
-import { RxCheckCircled, RxCheckbox, RxFile, RxQuestionMark, RxTokens } from 'react-icons/rx';
-import Profile from '../profile/profile';
-import Loadingrectangle from '../loading/loading';
+import axios from "axios";
+import Loadingrectangle from "../loading/loading"
+import Document from "./Document";
 
-const Files = ({ PortToUse }) => {
-  const [Documents, setDocuments] = useState([]);
-  const [Carrers, setCarrers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // Estado de carga
+const Files = ({PortToUse}) =>{
+    const [AllDocuments, setAllDocuments] = useState([]);
+    const [VisibleDocuments, setVisibleDocuments] = useState([]);
+    const [isLoading, setisLoading] = useState(true);
 
-  const fetchDocuments = async () => {
-    const response = await axios.get(PortToUse + "api/documents/", {
-      withCredentials: true,
-    });
-    setDocuments(response.data);
-  };
-
-  const fetchCarrers = async () => {
-    const response = await axios.get(PortToUse + "api/carrers/", {
-      withCredentials: true,
-    });
-    setCarrers(response.data);
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await Promise.all([fetchDocuments(), fetchCarrers()]);
-      setIsLoading(false); // Termina la carga después de obtener los datos
+    const fetchDocuments = async() =>{
+      try{
+          const response = await axios.get(PortToUse+"api/documents/",{
+            withCredentials:true,
+          });
+          setAllDocuments(response.data);
+          localStorage.setItem('documents', JSON.stringify(response.data));
+          setVisibleDocuments(response.data.slice(0,30));
+      }catch(error){
+        console.error('Error Trying To Fetch Documents: ',error );
+      };
     };
 
-    fetchData();
-  }, []);
+    useEffect(()=>{
+      const storedDocuments = localStorage.getItem("documents");
 
-  const handleDocumentClick = (documentid) => {
-    console.log("CLICKED THE DOCUMENT : ", documentid);
-  };
+      if (storedDocuments){
+        const parsedDocuments = JSON.parse(storedDocuments);
+        setAllDocuments(parsedDocuments);
+        setVisibleDocuments(parsedDocuments.slice(0,30));
+        setisLoading(false);
 
-  if (isLoading) {
-    return <Loadingrectangle />; // Muestra el componente de carga mientras se cargan los datos
-  }
 
-  return (
-    <div id='totaldocumentscontainer'>
-      {/* <Profile /> */}
-      
-      {Documents.map((document) => (
-        <div id="FileComponent" key={document.id}>
-          <div className='ThesisContainer'>
-            <div id='kind'>
-              <RxTokens id='logo' />
-              {document.code} {'\u00A0'} / {'\u00A0'} {document.carrer_name}
-            </div>
-            <div className='Title'>{document.title}</div>
-            <div className='Description'> {document.description}</div>
-            <div className='Author'>
-              <div>Author:</div>
-              {document.authors.map((author, index) => (
-                <span key={index}> {author}{'\u00A0'}-{'\u00A0'}</span>
-              ))}
-            </div>
-            <div className='DownloadButton'>
-              <a id='DownloadText' href={document.url}>
-                <RxFile className='icondoc' /> Accede Ahora (Libro Electrónico)
-              </a>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+      }
+      else{
+        const fetchData = async () =>{
+          await fetchDocuments()
+          setisLoading(false)
+
+        }
+        fetchData()
+      }
+
+    },[])
+    
+    const handleLoadMore = () =>{
+      setVisibleDocuments(AllDocuments.slice(0,VisibleDocuments.length + 30));
+    };
+
+    if (isLoading){
+      return <Loadingrectangle></Loadingrectangle>
+    }
+
+    // console.log(VisibleDocuments)
+
+
+return (
+  <div id='totaldocumentscontainer'>
+    {VisibleDocuments.map((document) => (
+      <Document key={document.id} document={document} />
+    ))}
+
+    {VisibleDocuments.length < AllDocuments.length && (
+      <button onClick={handleLoadMore} className="load-more-button">Cargar más</button>
+    )}
+  </div>
+);
 };
+
 
 export default Files;
