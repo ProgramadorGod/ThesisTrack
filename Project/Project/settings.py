7fa062e3df448dfac2116458345af396375ca67b
ProgramadorGod <pipenet12@gmail.com>
2024-07-21 09:06:23 -0500

    ??

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -122,8 +122,14 @@ CORS_ORIGIN_WHITELIST = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ttdatabase',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'3306'
+
+
     }
 }
 
