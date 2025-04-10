f4e65ea9bdaf70bb10053cb4ef26105f63009aa9
SrRiuz <mateo.jimenez@alternova.com>
2025-04-10 00:04:03 -0500

    feat/docker-setup-config

--- Project/Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Project.wsgi
--- Project/userz/models.py
+++ Project/userz/models.py
@@ -1,32 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class UserType(models.Model):
     UserType = models.CharField(max_length=30)
 
     def __str__(self):
-        return (self.UserType)
-    
+        return self.UserType
 
 
 class Account(AbstractUser):
-    UserType  = models.ForeignKey(UserType, on_delete=models.SET_DEFAULT, default=1)
+    #UserType = models.ForeignKey(UserType, on_delete=models.SET_DEFAULT, default=1)
     stars_given = models.IntegerField(default=0)
     documents_uploaded = models.IntegerField(default=0)
     documents_downloaded = models.IntegerField(default=0)
     comments_made = models.IntegerField(default=0)
     feedback_received = models.IntegerField(default=0)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True, default="profile_pictures/default.jpg")  # Nuevo campo
-
+    profile_picture = models.ImageField(upload_to="profile_pictures/", blank=True, null=True, default="profile_pictures/default.jpg")  # Nuevo campo
 
     def __str__(self) -> str:
-        return (self.username)
-    
-    
-    
+        return self.username
+
     def get_profile_picture_url(self):
         """Devuelve la URL de la imagen de perfil o una por defecto si no hay ninguna."""
         if self.profile_picture:
             return self.profile_picture.url
-        return "/media/profile_pictures/default.jpg"  
-    
+        return "/media/profile_pictures/default.jpg"
